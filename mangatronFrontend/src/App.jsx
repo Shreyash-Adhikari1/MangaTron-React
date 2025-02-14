9e942c68522dffcb1be4e07b0c05e402dc2dcea1
Shreyash-Adhikari1 <adhikarishreyash@gmail.com>
2025-02-14 12:17:42 +0545

    Changes

--- mangatronFrontend/src/App.jsx
+++ mangatronFrontend/src/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const Login = lazy(() => import("./components/public/Login.jsx"));
 const Registration = lazy(() => import("./components/public/Registration.jsx"));
 const Home = lazy(()=> import('./components/public/Home.jsx'));
-const Test = lazy(()=> import('./components/public/urlTest.jsx'))
 
 function App() {
   return (
