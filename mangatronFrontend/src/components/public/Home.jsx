3583b75ecb713986ac8ae35cd9422470e09398ff
Shreyash-Adhikari1 <adhikarishreyash@gmail.com>
2025-02-16 12:16:34 +0545

    Home Page Design and Components Complete

--- mangatronFrontend/src/components/public/Home.jsx
+++ mangatronFrontend/src/components/public/Home.jsx
@@ -36,6 +36,12 @@ import jigokuraku from '../../../public/assets/mangaImages/jigokuraku.jpg';
 import readerVp from '../../../public/assets/mangaImages/readersVp.jpg';
 import slam from '../../../public/assets/mangaImages/slam.jpg';
 import vagabond from '../../../public/assets/mangaImages/vagabond.jpg';
+import lookism from '../../../public/assets/mangaImages/lookism.jpg'
+import gokurakugai from '../../../public/assets/mangaImages/gokurakugai.jpeg'
+import opm from '../../../public/assets/mangaImages/opm.jpeg'
+import dad from '../../../public/assets/mangaImages/dad.jpg'
+import out from '../../../public/assets/mangaImages/out.jpg'
+import homunculus from '../../../public/assets/mangaImages/homunculus.jpeg'
 
 export default function Home() {
   const [selectedUrl, setSelectedUrl] = useState(null);
@@ -47,7 +53,10 @@ export default function Home() {
     { name: "Baskerville", img: baskerville, url: "https://weebcentral.com/series/01J76XYG95PJHF8P3PGVKJT5NQ/Return-of-the-Iron-Blooded-Hound" },
     { name: "Magic Emperor", img: magicEmperor, url: "https://weebcentral.com/series/01J76XYDT3Z0ZJ7NNG4Q74QFFG/Magic-Emperor" },
     { name: "Kagurabachi", img: kagurabachi, url: "https://weebcentral.com/series/01J76XYGGM22WZP7T4TKA4ZFAF/Kagurabachi" },
-    { name: "Omniscient Reader", img: readerVp, url: "https://weebcentral.com/series/01J76XYDMWWJX3T4BFWR84FQQK/Omniscient-Readers-Viewpoint" }
+    { name: "Omniscient Reader", img: readerVp, url: "https://weebcentral.com/series/01J76XYDMWWJX3T4BFWR84FQQK/Omniscient-Readers-Viewpoint" },
+    { name: "Lookism", img:lookism, url: "https://weebcentral.com/series/01J76XYBD282Y3XKX0KRGD64Q5/Lookism" },
+    { name: "Gokurakugai", img: gokurakugai, url: "https://weebcentral.com/series/01J76XYFNQGKHPRGEJYNZF3A6E/Gokurakugai" },
+    { name: "One-Punch Man", img: opm, url: "https://weebcentral.com/series/01J76XY7KT7J224EBK6J816Y1Q/Onepunch-Man" },
   ];
 
   const recommendedItems = [
@@ -58,6 +67,9 @@ export default function Home() {
     { name: "Hells Paradise", img: jigokuraku, url: "https://weebcentral.com/series/01J76XYCAJ8MJ5VECWQJE5QTP2/Jigokuraku-kaku-Yuuji" },
     { name: "Slam Dunk", img: slam, url: "https://weebcentral.com/series/01J76XY7GN9SV2VK3RTTCYQBEX/Slam-Dunk" },
     { name: "Omniscient Reader", img: readerVp, url: "https://weebcentral.com/series/01J76XYDMWWJX3T4BFWR84FQQK/Omniscient-Readers-Viewpoint" },
+    { name: "Peerless Dad", img:dad , url: "https://weebcentral.com/series/01J76XYCXXB105QEZ4J82JBAEB/Peerless-Dad" },
+    { name: "Out", img:out , url: "https://weebcentral.com/series/01J76XYDPQ762VGZBA5X2QK8SJ/Out-MIZUTA-Makoto" },
+    { name: "Homunculus", img:homunculus , url: "https://weebcentral.com/series/01J76XY9FNA8SMJ003M6NPYZA0/Homunculus" }
   ];
 
   return (
